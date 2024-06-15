6716393a893b8e297ab8d9da12f7d114b9fca59e
Tom Lienard <tom.lienrd@gmail.com>
2024-06-15 17:43:28 +0100

    fix: use `express.json()` & disable Node.js helpers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-const express = require("express"); 
+const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.post("/greet", (req, res) => { 
     res.send(req.body); 
